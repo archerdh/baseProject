ac26d80bd1050da8bad92b5d14f10c470b3af42f
wanli <archerdh@126.com>
2018-09-11 21:34:52 +0800

    add cell animation file

---
+++ DemoCode/Class/cellAnimation/DBCellAnimationViewController.h
@@ -0,0 +1,13 @@
+//
+//  DBCellAnimationViewController.h
+//  DemoCode
+//
+//  Created by zhangzheng on 2018/9/11.
+//  Copyright © 2018年 auction. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface DBCellAnimationViewController : BaseViewController
+
+@end
---
+++ DemoCode/Class/cellAnimation/DBCellAnimationViewController.m
@@ -0,0 +1,37 @@
+//
+//  DBCellAnimationViewController.m
+//  DemoCode
+//
+//  Created by zhangzheng on 2018/9/11.
+//  Copyright © 2018年 auction. All rights reserved.
+//
+
+#import "DBCellAnimationViewController.h"
+
+@interface DBCellAnimationViewController ()
+
+@end
+
+@implementation DBCellAnimationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
